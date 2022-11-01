25bca771d3e905c08f6ec5ebc08bf6109c505754
stormytuna <stormytuna@outlook.com>
2022-11-01 19:57:49 +0000

    Fix formatting

--- stormytunaUtils.cs
+++ stormytunaUtils.cs
@@ -1,13 +1,13 @@
-using Terraria;
 using Microsoft.Xna.Framework;
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using Terraria;
+using Terraria.ID;
 using Terraria.Localization;
 
-namespace ThreatOfPrecipitation
-{
-    public static class stormytunaUtils
-    {
+namespace ThreatOfPrecipitation {
+    public static class stormytunaUtils {
 
         /// <summary>Gets a list of NPCs within the range of that position</summary>
         /// <param name="position">The position, should be the center of the search and usually the center of another entity</param>
@@ -16,27 +16,23 @@ namespace ThreatOfPrecipitation
         /// <param name="careAboutCanBeChased">Whether the function should check npc.chaseable</param>
         /// <param name="excludedNPCs">The whoAmI fields of any NPCs that are excluded from the search</param>
         /// <returns>A list of NPCs within range of the position</returns>
-        public static List<NPC> GetNearbyEnemies(Vector2 position, float range, bool careAboutLineOfSight, bool careAboutCanBeChased,  List<int> excludedNPCs = null)
-        {
+        public static List<NPC> GetNearbyEnemies(Vector2 position, float range, bool careAboutLineOfSight, bool careAboutCanBeChased, List<int> excludedNPCs = null) {
             List<NPC> npcs = new List<NPC>();
             float rangeSquared = range * range;
             if (excludedNPCs == null)
                 excludedNPCs = new List<int>();
 
-            for (int i = 0; i < Main.npc.Length; i++)
-            {
+            for (int i = 0; i < Main.npc.Length; i++) {
                 NPC npc = Main.npc[i];
 
-                if (!npc.active || npc.CountsAsACritter || npc.friendly || !npc.immortal || excludedNPCs.Contains(npc.whoAmI))
-                {
+                if (!npc.active || npc.CountsAsACritter || npc.friendly || !npc.immortal || excludedNPCs.Contains(npc.whoAmI)) {
                     continue;
                 }
 
                 float distanceSquared = Vector2.DistanceSquared(position, npc.Center);
                 bool canSee = careAboutLineOfSight ? Collision.CanHit(position, 1, 1, npc.position, npc.width, npc.height) : true;
                 bool canBeChased = careAboutCanBeChased ? npc.chaseable : true;
-                if (distanceSquared <= rangeSquared && canSee && canBeChased)
-                {
+                if (distanceSquared <= rangeSquared && canSee && canBeChased) {
                     npcs.Add(npc);
                 }
             }
@@ -51,27 +47,23 @@ namespace ThreatOfPrecipitation
         /// <param name="careAboutCanBeChased">Whether the function should check npc.chaseable</param>
         /// <param name="excludedNPCs">The whoAmI fields of any NPCs that are excluded from the search</param>
         /// <returns>Returns the closest NPC. Returns null if no NPC is found</returns>
-        public static NPC GetClosestEnemy(Vector2 position, float range, bool careAboutLineOfSight, bool careAboutCanBeChased, List<int> excludedNPCs = null)
-        {
+        public static NPC GetClosestEnemy(Vector2 position, float range, bool careAboutLineOfSight, bool careAboutCanBeChased, List<int> excludedNPCs = null) {
             NPC closestNPC = null;
             float rangeSquared = range * range;
             if (excludedNPCs == null)
                 excludedNPCs = new List<int>();
 
-            for (int i = 0; i < Main.npc.Length; i++)
-            {
+            for (int i = 0; i < Main.npc.Length; i++) {
                 NPC npc = Main.npc[i];
 
-                if (!npc.active || npc.CountsAsACritter || npc.friendly || !npc.immortal || excludedNPCs.Contains(npc.whoAmI))
-                { 
+                if (!npc.active || npc.CountsAsACritter || npc.friendly || !npc.immortal || excludedNPCs.Contains(npc.whoAmI)) {
                     continue;
                 }
 
                 float distanceSquared = Vector2.DistanceSquared(position, npc.Center);
                 bool canSee = careAboutLineOfSight ? Collision.CanHit(position, 1, 1, npc.position, npc.width, npc.height) : true;
                 bool canBeChased = careAboutCanBeChased ? npc.chaseable : true;
-                if (distanceSquared < rangeSquared && canSee && canBeChased)
-                {
+                if (distanceSquared < rangeSquared && canSee && canBeChased) {
                     closestNPC = npc;
                     rangeSquared = distanceSquared;
                 }
@@ -87,26 +79,22 @@ namespace ThreatOfPrecipitation
         /// <param name="team">The team the player should match. 0 means team doesn't matter</param>
         /// <param name="excludedPlayers">The whoAmI fields of any players that are excluded from the search</param>
         /// <returns>A list of players within range of the position</returns>
-        public static List<Player> GetNearbyPlayers(Vector2 position, float range, bool careAboutLineOfSight, int team = 0, List<int> excludedPlayers = null)
-        {
+        public static List<Player> GetNearbyPlayers(Vector2 position, float range, bool careAboutLineOfSight, int team = 0, List<int> excludedPlayers = null) {
             List<Player> players = new List<Player>();
             float rangeSquared = range * range;
             if (excludedPlayers == null)
                 excludedPlayers = new List<int>();
 
-            for (int i = 0; i < Main.player.Length; i++)
-            {
+            for (int i = 0; i < Main.player.Length; i++) {
                 Player player = Main.player[i];
 
-                if (!player.active || player.dead || (player.team != team && team != 0) || excludedPlayers.Contains(player.whoAmI))
-                {
+                if (!player.active || player.dead || (player.team != team && team != 0) || excludedPlayers.Contains(player.whoAmI)) {
                     continue;
                 }
 
                 float distanceSquared = Vector2.DistanceSquared(position, player.Center);
                 bool canSee = careAboutLineOfSight ? Collision.CanHit(position, 1, 1, player.position, player.width, player.height) : true;
-                if (distanceSquared <= rangeSquared && canSee)
-                {
+                if (distanceSquared <= rangeSquared && canSee) {
                     players.Add(player);
                 }
             }
@@ -122,8 +110,7 @@ namespace ThreatOfPrecipitation
         /// <param name="rotationMax">The max amount of degrees the velocity can be rotated, make sure to use degrees as this function coverts it to radians</param>
         /// <param name="excludeNPCs">The whoAmI fields of any NPCs that are excluded from the search</param>
         /// <returns>Returns velocity rotated towards the found NPC. Returns the original velocity if no NPC is found</returns>
-        public static Vector2 RotateVelocityHoming(Vector2 currentVelocity, Vector2 startPosition, float range, bool careAboutLineOfSight, float rotationMax, List<int> excludeNPCs = null)
-        {
+        public static Vector2 RotateVelocityHoming(Vector2 currentVelocity, Vector2 startPosition, float range, bool careAboutLineOfSight, float rotationMax, List<int> excludeNPCs = null) {
             if (excludeNPCs == null)
                 excludeNPCs = new List<int>();
 
@@ -141,8 +128,7 @@ namespace ThreatOfPrecipitation
         /// <param name="targetPosition">The target position, should be the center of the target</param>
         /// <param name="rotationMax">The max amount of degrees the velocity can be rotated, make sure to use degrees as this function coverts it to radians</param>
         /// <returns>Returns velocity rotated towards the given position</returns>
-        public static Vector2 RotateVelocityHoming(Vector2 currentVelocity, Vector2 startPosition, Vector2 targetPosition, float rotationMax)
-        {
+        public static Vector2 RotateVelocityHoming(Vector2 currentVelocity, Vector2 startPosition, Vector2 targetPosition, float rotationMax) {
             rotationMax = MathHelper.ToRadians(rotationMax);
             float rotTarget = Utils.ToRotation(targetPosition - startPosition);
             float rotCurrent = Utils.ToRotation(currentVelocity);
@@ -154,8 +140,7 @@ namespace ThreatOfPrecipitation
         /// <param name="rotationTestAgainst">The rotation to test against, measured in radians</param>
         /// <param name="range">The range around rotation to test if rotationTestAgainst is within, checks either side with the raw range (ie it isn't halved), measured in radians</param>
         /// <returns>Returns true if rotationTestAgainst is within range of rotation. Returns false otherwise</returns>
-        public static bool RotationIsWithinRange(float rotation, float rotationTestAgainst, float range)
-        {
+        public static bool RotationIsWithinRange(float rotation, float rotationTestAgainst, float range) {
             float absDif = MathF.Abs(rotation - rotationTestAgainst);
             return absDif < range;
         }
@@ -165,8 +150,7 @@ namespace ThreatOfPrecipitation
         /// <param name="end">The ending value, will return this when amount == 1</param>
         /// <param name="amount">The amount to slerp by</param>
         /// <returns>Returns the spherical interpolation between start and end</returns>
-        public static float Slerp(float start, float end, float amount)
-        {
+        public static float Slerp(float start, float end, float amount) {
             if (amount == 0f)
                 return start;
             if (amount == 1f)
@@ -186,8 +170,7 @@ namespace ThreatOfPrecipitation
         /// <param name="amount">The amount to lerp by</param>
         /// <param name="exponent">The exponent of the easing curve to use, larger values cause more easing</param>
         /// <returns>Returns the ease in interpolation between start and end</returns>
-        public static float EaseIn(float start, float end, float amount, int exponent)
-        {
+        public static float EaseIn(float start, float end, float amount, int exponent) {
             if (amount == 0f)
                 return start;
             if (amount == 1f)
@@ -205,8 +188,7 @@ namespace ThreatOfPrecipitation
         /// <param name="amount">The amount to lerp by</param>
         /// <param name="exponent">The exponent of the easing curve to use, larger values cause more easing</param>
         /// <returns>Returns the ease out interpolation between start and end</returns>
-        public static float EaseOut(float start, float end, float amount, int exponent)
-        {
+        public static float EaseOut(float start, float end, float amount, int exponent) {
             if (amount == 0f)
                 return start;
             if (amount == 1f)
@@ -222,8 +204,7 @@ namespace ThreatOfPrecipitation
         /// <summary>Sets the magnitude of the vector to the given magnitude</summary>
         /// <param name="vector">The vector to be changed</param>
         /// <param name="magnitude">The new magnitude of the vector</param>
-        public static void SetMagnitude(this Vector2 vector, float magnitude)
-        {
+        public static void SetMagnitude(this Vector2 vector, float magnitude) {
             vector.Normalize();
             vector *= magnitude;
         }
@@ -233,42 +214,34 @@ namespace ThreatOfPrecipitation
         /// <param name="useIcons">Whether the string should use coin icons or coin names</param>
         /// <param name="useColors">Whether the string should colour the number of coins</param>
         /// <returns>Returns a string representing the coin value</returns>
-        public static string CoinValueToString(int coinValue, bool useIcons, bool useColors)
-        {
+        public static string CoinValueToString(int coinValue, bool useIcons, bool useColors) {
             // Code copied from vanilla - PopupText.ValueToName()
             int platinumCoins = 0;
             int goldCoins = 0;
             int silverCoins = 0;
             int copperCoins = 0;
-            while (coinValue > 0)
-            {
-                if (coinValue >= 1000000)
-                {
+            while (coinValue > 0) {
+                if (coinValue >= 1000000) {
                     coinValue -= 1000000;
                     platinumCoins++;
                 }
-                else if (coinValue >= 10000)
-                {
+                else if (coinValue >= 10000) {
                     coinValue -= 10000;
                     goldCoins++;
                 }
-                else if (coinValue >= 100)
-                {
+                else if (coinValue >= 100) {
                     coinValue -= 100;
                     silverCoins++;
                 }
-                else if (coinValue >= 1)
-                {
+                else if (coinValue >= 1) {
                     coinValue--;
                     copperCoins++;
                 }
             }
 
             string text = "";
-            if (!useIcons)
-            {
-                if (!useColors)
-                {
+            if (!useIcons) {
+                if (!useColors) {
                     if (platinumCoins > 0)
                         text = text + platinumCoins + string.Format(" {0} ", Language.GetTextValue("Currency.Platinum"));
 
@@ -281,8 +254,7 @@ namespace ThreatOfPrecipitation
                     if (copperCoins > 0)
                         text = text + copperCoins + string.Format(" {0} ", Language.GetTextValue("Currency.Copper"));
                 }
-                else
-                {
+                else {
                     if (platinumCoins > 0)
                         text = text + $"[c/DCDCC6:{platinumCoins}]" + string.Format(" {0} ", Language.GetTextValue("Currency.Platinum"));
 
@@ -296,10 +268,8 @@ namespace ThreatOfPrecipitation
                         text = text + $"[c/F68A60:{copperCoins}]" + string.Format(" {0} ", Language.GetTextValue("Currency.Copper"));
                 }
             }
-            else
-            {
-                if (!useColors)
-                {
+            else {
+                if (!useColors) {
                     if (platinumCoins > 0)
                         text = text + platinumCoins + "[i:74] ";
 
@@ -312,8 +282,7 @@ namespace ThreatOfPrecipitation
                     if (copperCoins > 0)
                         text = text + copperCoins + "[i:71] ";
                 }
-                else
-                {
+                else {
                     if (platinumCoins > 0)
                         text = text + $"[c/DCDCC6:{platinumCoins}]" + "[i:74] ";
 
