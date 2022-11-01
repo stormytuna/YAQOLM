385209c600e8518ebc93f90252031fe1508c0f61
stormytuna <stormytuna@outlook.com>
2022-11-01 19:57:58 +0000

    Add AddToShop method to utils class

--- stormytunaUtils.cs
+++ stormytunaUtils.cs
@@ -302,5 +302,53 @@ namespace ThreatOfPrecipitation {
 
             return text;
         }
+
+        /// <summary>Adds an item to a shop</summary>
+        /// <param name="shop">The <c>shop</c> parameter of <c>SetupShop</c></param>
+        /// <param name="nextSlot">The <c>nextSlot</c> parameter of <c>SetupShop</c>, no need to increment this yourself as this function does that if needed</param>
+        /// <param name="itemType">The type of the item being inserted</param>
+        /// <param name="itemAfterType">The type of an item already in the shop that an item is inserted after</param>
+        /// <param name="defaultIndex">The default index if an item with type <c>itemAfterType</c> isn't found</param>
+        public static void AddToShop(ref Chest shop, ref int nextSlot, int itemType, int itemAfterType, int defaultIndex) {
+            Predicate<Item> pred = delegate (Item i) { return i.type == itemAfterType; };
+            AddToShop(ref shop, ref nextSlot, itemType, pred, defaultIndex);
+        }
+
+        /// <summary>Adds an item to a shop</summary>
+        /// <param name="shop">The <c>shop</c> parameter of <c>SetupShop</c></param>
+        /// <param name="nextSlot">The <c>nextSlot</c> parameter of <c>SetupShop</c>, no need to increment this yourself as this function does that if needed</param>
+        /// <param name="itemType">The type of the item being inserted</param>
+        /// <param name="after">The predicate an Item must match for the added item to be inserted after it</param>
+        /// <param name="defaultIndex">The default index if an item with type <c>itemAfterType</c> isn't found</param>
+        public static void AddToShop(ref Chest shop, ref int nextSlot, int itemType, Predicate<Item> after, int defaultIndex) {
+            // Get our inventory as a list
+            List<Item> inventory = shop.item.ToList();
+
+            // Get our correct index
+            Item itemAfter = inventory.FirstOrDefault(i => after.Invoke(i));
+            int index = defaultIndex;
+            if (itemAfter != null)
+                index = inventory.IndexOf(itemAfter) + 1;
+
+            // Check itemType isn't already in our shop
+            Item item = inventory.FirstOrDefault(i => i.type == itemType);
+            if (item != null) {
+                // Move it if it is
+                inventory.Remove(item);
+                inventory.Insert(index, item);
+
+                // Reassign our item array as ToList doesnt provide a reference
+                shop.item = inventory.ToArray();
+
+                return;
+            }
+
+            // Add our item since it isn't here
+            item = ContentSamples.ItemsByType[itemType];
+            inventory.Insert(index, item);
+            nextSlot++;
+
+            shop.item = inventory.ToArray();
+        }
     }
 }
