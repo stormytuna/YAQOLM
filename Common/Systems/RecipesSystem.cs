stormytuna/YAQOLM

commit a27fc41ab946644041a6ddb62a799260c2cadf7d

Add arrays system and update everything to use it
Just collects all int[] into one place

Common/Systems/RecipesSystem.cs
@@ -235,48 +235,13 @@ namespace YAQOLM.Common.Systems {
             }
         }
 
-        private int[] barTypes = new int[] {
-            ItemID.CopperBar,
-            ItemID.TinBar,
-            ItemID.IronBar,
-            ItemID.LeadBar,
-            ItemID.SilverBar,
-            ItemID.TungstenBar,
-            ItemID.GoldBar,
-            ItemID.PlatinumBar,
-            ItemID.MeteoriteBar,
-            ItemID.DemoniteBar,
-            ItemID.CrimtaneBar,
-            ItemID.HellstoneBar
-        };
 
-        private int[] oreTypes = new int[] {
-            ItemID.CopperOre,
-            ItemID.TinOre,
-            ItemID.IronOre,
-            ItemID.LeadOre,
-            ItemID.SilverOre,
-            ItemID.TungstenOre,
-            ItemID.GoldOre,
-            ItemID.PlatinumOre,
-            ItemID.Meteorite,
-            ItemID.DemoniteOre,
-            ItemID.CrimtaneOre,
-            ItemID.Hellstone
-        };
-
-        private int[] beetleArmor = new int[] {
-            ItemID.BeetleHelmet,
-            ItemID.BeetleLeggings,
-            ItemID.BeetleScaleMail,
-            ItemID.BeetleShell
-        };
 
         public override void PostAddRecipes() {
             if (ServerConfig.Instance.CheaperOre) {
                 for (int i = 0; i < Main.recipe.Length; i++) {
                     Recipe recipe = Main.recipe[i];
-                    if (barTypes.Contains(recipe.createItem.type) && !recipe.requiredTile.Contains(TileID.AdamantiteForge) && oreTypes.Contains(recipe.requiredItem[0].type)) {
+                    if (ArraySystem.BarTypes.Contains(recipe.createItem.type) && !recipe.requiredTile.Contains(TileID.AdamantiteForge) && ArraySystem.OreTypes.Contains(recipe.requiredItem[0].type)) {
                         recipe.AddCondition(NetworkText.FromLiteral("Pre Hardmode Furnace"), r => !Main.LocalPlayer.adjTile[TileID.AdamantiteForge]);
                     }
                 }
@@ -284,7 +249,7 @@ namespace YAQOLM.Common.Systems {
 
             if (ServerConfig.Instance.BeetleArmorOnlyBeetle) {
                 for (int i = 0; i < Main.recipe.Length; i++) {
-                    if (beetleArmor.Contains(Main.recipe[i].createItem.type) && Main.recipe[i].requiredItem.Count > 1) {
+                    if (ArraySystem.BeetleArmor.Contains(Main.recipe[i].createItem.type) && Main.recipe[i].requiredItem.Count > 1) {
                         Main.recipe[i].DisableRecipe();
                     }
                 }

Common/GlobalNPCs/ShopGlobalNPC.cs
@@ -4,20 +4,12 @@ using Terraria;
 using Terraria.Enums;
 using Terraria.ID;
 using Terraria.ModLoader;
-using ThreatOfPrecipitation;
 using YAQOLM.Common.Configs;
+using YAQOLM.Common.Systems;
 
 namespace YAQOLM.Common.GlobalNPCs {
     public class ShopGlobalNPC : GlobalNPC {
         public override bool InstancePerEntity => true;
-
-        private readonly int[] solutionIds = new int[] {
-            ItemID.GreenSolution,
-            ItemID.PurpleSolution,
-            ItemID.RedSolution,
-            ItemID.BlueSolution,
-            ItemID.DarkBlueSolution
-        };
 
         public override void SetupShop(int type, Chest shop, ref int nextSlot) {
             if (type == NPCID.Steampunker && ServerConfig.Instance.SteampunkerSolutions) {
@@ -29,8 +21,8 @@ namespace YAQOLM.Common.GlobalNPCs {
                     index = inventory.IndexOf(firstSolution) + 1;
                 }
 
-                for (int i = 0; i < solutionIds.Length; i++) {
-                    int solutionId = solutionIds[i];
+                for (int i = 0; i < ArraySystem.SolutionIds.Length; i++) {
+                    int solutionId = ArraySystem.SolutionIds[i];
                     Item solution = inventory.FirstOrDefault(i => i.type == solutionId);
                     // Edge case - solution we found is our first solution, just continue in this case
                     if (solution == firstSolution) {

Common/Systems/ArraysSystem.cs
@@ -1,0 +1,85 @@
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace YAQOLM.Common.Systems {
+    public class ArraySystem : ModSystem {
+        private static int[] _solutionIds = new int[] {
+            ItemID.GreenSolution,
+            ItemID.PurpleSolution,
+            ItemID.RedSolution,
+            ItemID.BlueSolution,
+            ItemID.DarkBlueSolution
+        };
+
+        private static int[] _barTypes = new int[] {
+            ItemID.CopperBar,
+            ItemID.TinBar,
+            ItemID.IronBar,
+            ItemID.LeadBar,
+            ItemID.SilverBar,
+            ItemID.TungstenBar,
+            ItemID.GoldBar,
+            ItemID.PlatinumBar,
+            ItemID.MeteoriteBar,
+            ItemID.DemoniteBar,
+            ItemID.CrimtaneBar,
+            ItemID.HellstoneBar
+        };
+
+        private static int[] _oreTypes = new int[] {
+            ItemID.CopperOre,
+            ItemID.TinOre,
+            ItemID.IronOre,
+            ItemID.LeadOre,
+            ItemID.SilverOre,
+            ItemID.TungstenOre,
+            ItemID.GoldOre,
+            ItemID.PlatinumOre,
+            ItemID.Meteorite,
+            ItemID.DemoniteOre,
+            ItemID.CrimtaneOre,
+            ItemID.Hellstone
+        };
+
+        private static int[] _beetleArmor = new int[] {
+            ItemID.BeetleHelmet,
+            ItemID.BeetleLeggings,
+            ItemID.BeetleScaleMail,
+            ItemID.BeetleShell
+        };
+
+        private static int[] itemsWithLegendary2 = new int[] {
+            ItemID.Terrarian,
+            ItemID.CopperShortsword,
+            ItemID.Gladius,
+            ItemID.GoldShortsword,
+            ItemID.IronShortsword,
+            ItemID.LeadShortsword,
+            ItemID.PlatinumShortsword,
+            ItemID.Ruler,
+            ItemID.SilverShortsword,
+            ItemID.TinShortsword,
+            ItemID.TragicUmbrella,
+            ItemID.TungstenShortsword,
+            ItemID.Umbrella
+        };
+
+        public static int[] SolutionIds { get => _solutionIds; }
+
+        public static int[] BarTypes { get => _barTypes; }
+
+        public static int[] OreTypes { get => _oreTypes; }
+
+        public static int[] BeetleArmor { get => _beetleArmor; }
+
+        public static int[] ItemsWithLegendary2 { get => itemsWithLegendary2; }
+
+        public override void Unload() {
+            _solutionIds = null;
+            _barTypes = null;
+            _oreTypes = null;
+            _beetleArmor = null;
+            itemsWithLegendary2 = null;
+        }
+    }
+}

Common/Systems/PrefixSystem.cs
@@ -6,22 +6,6 @@ using Terraria.ModLoader;
 
 namespace YAQOLM.Common.Systems {
     public static class PrefixSystem {
-        private static readonly int[] itemsWithLegendary2 = new int[] {
-            ItemID.Terrarian,
-            ItemID.CopperShortsword,
-            ItemID.Gladius,
-            ItemID.GoldShortsword,
-            ItemID.IronShortsword,
-            ItemID.LeadShortsword,
-            ItemID.PlatinumShortsword,
-            ItemID.Ruler,
-            ItemID.SilverShortsword,
-            ItemID.TinShortsword,
-            ItemID.TragicUmbrella,
-            ItemID.TungstenShortsword,
-            ItemID.Umbrella
-        };
-
         private static int BestPrefix(Item item) {
             // Items with no knockback
             if (item.knockBack <= 0f) {
@@ -34,7 +18,7 @@ namespace YAQOLM.Common.Systems {
             }
 
             // Shortswords and terrarian
-            if (itemsWithLegendary2.Contains(item.type)) {
+            if (ArraySystem.ItemsWithLegendary2.Contains(item.type)) {
                 return PrefixID.Legendary2;
             }
 

commit 35754dcefb7f65e519f2d3b3b50fbd311966a6b2

Fix bug introduced in a27fc41ab946644041a6ddb62a799260c2cadf7d

Common/Systems/RecipesSystem.cs
@@ -24,9 +24,6 @@ namespace YAQOLM.Common.Systems {
             evilMaterialRecipeGroup = null;
             moonLordWeaponRecipeGroup = null;
             moonLordItemRecipeGroup = null;
-            beetleArmor = null;
-            oreTypes = null;
-            barTypes = null;
         }
 
         public override void AddRecipeGroups() {