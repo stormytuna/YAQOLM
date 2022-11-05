cb2a6f2888e2067a332bedd356fa69099de4c22b
stormytuna <stormytuna@outlook.com>
2022-11-05 17:07:17 +0000

    Add Todo comment

--- Common/Systems/RecipeSystem.cs
+++ Common/Systems/RecipeSystem.cs
@@ -100,6 +100,8 @@ namespace YAQOLM.Common.Systems {
                 recipe.AddIngredient(ItemID.Glass, 5);
                 recipe.AddTile(TileID.DemonAltar);
                 recipe.Register();
+
+                // TODO: remove this in 1.4.4
             }
 
             if (ServerConfig.Instance.LuminiteSmeltingRecipes) {
