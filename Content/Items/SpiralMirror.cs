b4a018ad7423578ff85269239652c1eb5684475e
stormytuna <stormytuna@outlook.com>
2022-12-14 14:58:36 +0000

    Add some stuff to spiral mirror

--- Content/Items/SpiralMirror.cs
+++ Content/Items/SpiralMirror.cs
@@ -11,7 +11,7 @@ namespace YAQOLM.Content.Items {
     public class SpiralMirror : ModItem {
         public override void SetStaticDefaults() {
             CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 1;
-            Tooltip.SetDefault("Use to teleport somewhere\nRight click to switch mode\nAllows you to teleport to party members, NPCs and Pylons");
+            Tooltip.SetDefault("Use to teleport somewhere\nRight click to switch mode\nAllows you to teleport to party members, NPCs and Pylons\nDisplays everything\n'Can I interest you in everything, all of the time?'");
         }
 
         public override void SetDefaults() {
@@ -27,7 +27,7 @@ namespace YAQOLM.Content.Items {
             Item.UseSound = SoundID.Item6;
         }
 
-        public override bool CanUseItem(Player player) => _mode == 1 ? player.GetModPlayer<WarpedMirrorPlayer>().deathLocation != Vector2.Zero : true;
+        public override bool CanUseItem(Player player) => _mode == 2 ? player.GetModPlayer<WarpedMirrorPlayer>().canUseWarpedMirror : true;
 
         public override void AddRecipes() {
             if (ServerConfig.Instance.SpiralMirror) {
@@ -35,6 +35,9 @@ namespace YAQOLM.Content.Items {
                     .AddIngredient(ModContent.ItemType<WarpedMirror>())
                     .AddIngredient(ModContent.ItemType<MysticMirror>())
                     .AddIngredient(ModContent.ItemType<RunicMirror>())
+                    .AddIngredient(ItemID.CellPhone)
+                    .AddIngredient(ItemID.MagicConch)
+                    .AddIngredient(ItemID.DemonConch)
                     .AddIngredient(ItemID.ChlorophyteBar, 3)
                     .AddTile(TileID.MythrilAnvil)
                     .Register();
@@ -45,8 +48,12 @@ namespace YAQOLM.Content.Items {
 
         public override void RightClick(Player player) {
             _mode++;
-            if (_mode > 2) {
+            if (_mode > 5) {
                 _mode = 0;
+            }
+            // TODO: remove this when teleport to world spawn is added
+            if (_mode == 1) {
+                _mode++;
             }
 
             Item.stack++;
@@ -61,18 +68,42 @@ namespace YAQOLM.Content.Items {
         }
 
         private void MakeDust(Player player) {
-            Dust d = null;
+            Dust d;
+            Vector2 vector;
             switch (_mode) {
                 case 0:
+                case 1:
                     d = Dust.NewDustDirect(player.position, player.width, player.height, DustID.MagicMirror, 0f, 0f, 150, default, 1.1f);
                     d.velocity *= 0.5f;
                     return;
-                case 1:
+                case 2:
                     Dust.NewDust(player.position, player.width, player.height, DustID.Demonite, 0f, 0f, 150, default, 1.1f);
                     return;
-                case 2:
+                case 3:
                     d = Dust.NewDustDirect(player.position, player.width, player.height, DustID.MagicMirror, 0f, 0f, 150, Color.Cyan, 1.1f);
                     d.velocity *= 0.5f;
+                    return;
+                case 4:
+                    vector = Vector2.UnitY.RotatedBy((float)player.itemAnimation * MathHelper.TwoPi / 30f) * new Vector2(15f, 0f);
+                    d = Dust.NewDustPerfect(player.Bottom + vector, Dust.dustWater());
+                    d.velocity.Y *= 0f;
+                    d.velocity.Y -= 4.5f;
+                    d.velocity.X *= 1.5f;
+                    d.scale = 0.8f;
+                    d.alpha = 130;
+                    d.noGravity = true;
+                    d.fadeIn = 1.1f;
+                    return;
+                case 5:
+                    vector = Vector2.UnitY.RotatedBy((float)player.itemAnimation * MathHelper.TwoPi / 30f) * new Vector2(15f, 0f);
+                    d = Dust.NewDustPerfect(player.Bottom + vector, 35);
+                    d.velocity.Y *= 0f;
+                    d.velocity.Y -= 4.5f;
+                    d.velocity.X *= 1.5f;
+                    d.scale = 0.8f;
+                    d.alpha = 130;
+                    d.noGravity = true;
+                    d.fadeIn = 1.1f;
                     return;
             }
         }
@@ -112,11 +143,30 @@ namespace YAQOLM.Content.Items {
                         player.Spawn(PlayerSpawnContext.RecallFromItem);
                         break;
                     case 1:
-                        player.Teleport(warpedPlayer.deathLocation, -1); // style: -1 prevents vanilla from doing any teleport effects
-                        warpedPlayer.deathLocation = Vector2.Zero;
+                        // TODO: Come back here and let it teleport you to world spawn
                         break;
                     case 2:
+                        player.Teleport(warpedPlayer.deathLocation, -1); // style: -1 prevents vanilla from doing any teleport effects
+                        warpedPlayer.canUseWarpedMirror = false;
+                        break;
+                    case 3:
                         player.DoPotionOfReturnTeleportationAndSetTheComebackPoint();
+                        break;
+                    case 4:
+                        if (Main.netMode == NetmodeID.SinglePlayer) {
+                            player.MagicConch();
+                        }
+                        else if (Main.netMode == NetmodeID.MultiplayerClient && Main.myPlayer == player.whoAmI) {
+                            NetMessage.SendData(MessageID.RequestTeleportationByServer, -1, -1, null, 1);
+                        }
+                        break;
+                    case 5:
+                        if (Main.netMode == NetmodeID.SinglePlayer) {
+                            player.DemonConch();
+                        }
+                        else if (Main.netMode == NetmodeID.MultiplayerClient && Main.myPlayer == player.whoAmI) {
+                            NetMessage.SendData(MessageID.RequestTeleportationByServer, -1, -1, null, 2);
+                        }
                         break;
                 }
 
@@ -135,21 +185,51 @@ namespace YAQOLM.Content.Items {
 
         public override void ModifyTooltips(List<TooltipLine> tooltips) {
             var tooltip = tooltips.FirstOrDefault(t => t.Name == "Tooltip0");
+            float colorMult = Main.mouseTextColor / 255f;
+
             switch (_mode) {
                 case 0:
-                    tooltip.Text = "Gaze into the mirror to return home";
+                    Color homeColor = new Color(51, 153, 255) * colorMult;
+                    tooltip.Text = $"Gaze into the mirror to return [c/{homeColor.Hex3()}:home]";
                     break;
                 case 1:
-                    tooltip.Text = "Gaze into the mirror to return to where you last died";
+                    Color spawnColor = new Color(0, 204, 0) * colorMult;
+                    tooltip.Text = $"Gaze into the mirror to return to [c/{spawnColor.Hex3()}:spawn]";
                     break;
                 case 2:
-                    tooltip.Text = "Gaze into the mirror to return home and create a portal";
+                    Color graveColor = new Color(255, 153, 51) * colorMult;
+                    tooltip.Text = $"Gaze into the mirror to return to [c/{graveColor.Hex3()}:where you last died]";
+                    break;
+                case 3:
+                    Color returnColor = new Color(204, 0, 204) * colorMult;
+                    tooltip.Text = $"Gaze into the mirror to return [c/{returnColor.Hex3()}:home and create a portal]";
+                    break;
+                case 4:
+                    Color oceanColor = new Color(51, 255, 255) * colorMult;
+                    tooltip.Text = $"Gaze into the mirror to travel to [c/{oceanColor.Hex3()}:the ocean]";
+                    break;
+                case 5:
+                    Color hellColor = new Color(204, 0, 0) * colorMult;
+                    tooltip.Text = $"Gaze into the mirror to travel to [c/{hellColor.Hex3()}:the underworld]";
                     break;
             }
         }
 
         public override void UpdateInventory(Player player) {
             player.GetModPlayer<RunicMirrorPlayer>().runicMirror = true;
+
+            player.accWatch = 3;
+            player.accDepthMeter = 1;
+            player.accCompass = 1;
+            player.accFishFinder = true;
+            player.accDreamCatcher = true;
+            player.accCritterGuide = true;
+            player.accOreFinder = true;
+            player.accStopwatch = true;
+            player.accCalendar = true;
+            player.accJarOfSouls = true;
+            player.accThirdEye = true;
+            player.accWeatherRadio = true;
         }
     }
 }
