63085b567687584e420ffe636321653d22f0c5ff
stormytuna <stormytuna@outlook.com>
2022-12-14 00:04:02 +0000

    Change magnificent magnet to have modes you cycle through

--- Content/Items/MagnificentMagnet.cs
+++ Content/Items/MagnificentMagnet.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using Microsoft.Xna.Framework;
+using System.Collections.Generic;
 using System.Linq;
 using Terraria;
 using Terraria.GameContent.Creative;
@@ -37,7 +38,7 @@ namespace YAQOLM.Content.Items {
             }
         }
 
-        private bool _disabled = false;
+        public int Mode { get; private set; } = 0;
 
         public override void ModifyTooltips(List<TooltipLine> tooltips) {
             // Get index
@@ -45,11 +46,31 @@ namespace YAQOLM.Content.Items {
             int index = tooltips.IndexOf(lastTip) + 1;
 
             // Make TooltipLine
-            string text = _disabled ? "[c/ff0000:Disabled:] Right click to enable" : "[c/00ff00:Enabled:] Right click to disable";
-            TooltipLine line = new(Mod, "EnabledDisabled", text);
+            string text = "";
+            Color color;
+            switch (Mode) {
+                case 0:
+                    color = Color.Red * Main.mouseTextColor;
+                    text = $"[c/{color.Hex3()}:Disabled]";
+                    break;
+                case 1:
+                    color = Color.Orange * Main.mouseTextColor;
+                    text = $"[c/{color.Hex3()}:1x strength]";
+                    break;
+                case 2:
+                    color = Color.Yellow * Main.mouseTextColor;
+                    text = $"[c/{color.Hex3()}:10x strength]";
+                    break;
+                case 3:
+                    color = Color.Green * Main.mouseTextColor;
+                    text = $"[c/{color.Hex3()}:100x strength]";
+                    break;
+            }
+            TooltipLine line = new(Mod, "Mode", text);
 
             // insert
             tooltips.Insert(index, line);
+            tooltips.Insert(index + 1, new(Mod, "RightClick", "Right click to change mode"));
         }
 
         public override bool CanRightClick() => true;
@@ -57,23 +78,59 @@ namespace YAQOLM.Content.Items {
         public override void OnConsumeItem(Player player) => Item.stack++;
 
         public override void RightClick(Player player) {
-            _disabled = !_disabled;
+            Mode++;
+            if (Mode > 3) {
+                Mode = 0;
+            }
         }
 
         public override bool? UseItem(Player player) {
             if (player.whoAmI == Main.myPlayer) {
-                for (int i = 0; i < Main.item.Length; i++) {
-                    Main.item[i].position = player.Center;
+                for (int i = 0; i < Main.maxItems; i++) {
+                    Item item = Main.item[i];
+
+                    if (!item.active || item.noGrabDelay != 0 || item.playerIndexTheItemIsReservedFor != player.whoAmI) {
+                        continue;
+                    }
+
+                    item.beingGrabbed = true;
+                    item.Center = player.Center;
                 }
             }
 
             return true;
         }
+    }
 
-        public override void UpdateInventory(Player player) {
-            if (!_disabled) {
-                player.treasureMagnet = true;
+    public class MagnificentMagnetPlayer : ModPlayer {
+        public override void Load() {
+            On.Terraria.Player.GetItemGrabRange += Player_GetItemGrabRange;
+        }
+
+        public override void Unload() {
+            On.Terraria.Player.GetItemGrabRange += Player_GetItemGrabRange;
+        }
+
+        private int Player_GetItemGrabRange(On.Terraria.Player.orig_GetItemGrabRange orig, Player self, Item item) {
+            int ret = orig(self, item);
+
+            int magnet = self.FindItem(ModContent.ItemType<MagnificentMagnet>());
+            if (magnet != -1) {
+                var modItem = self.inventory[magnet].ModItem as MagnificentMagnet;
+                switch (modItem.Mode) {
+                    case 1:
+                        ret += 150;
+                        break;
+                    case 2:
+                        ret += 1500;
+                        break;
+                    case 3:
+                        ret += 15000;
+                        break;
+                }
             }
+
+            return ret;
         }
     }
 }
