a37eda62eceef5268e901bbda87c6e35302cc048
stormytuna <stormytuna@outlook.com>
2023-06-11 11:59:50 +0100

    Clarify purpose of config items

--- Common/Configs/ConfigItems.cs
+++ Common/Configs/ConfigItems.cs
@@ -2,22 +2,29 @@ using Terraria.ModLoader;
 
 namespace YAQOLM.Common.Configs;
 
-public class _CONFIG_WarpedMirror : ModItem { }
+public abstract class ConfigItem : ModItem
+{
+	public override void SetStaticDefaults() {
+		Tooltip.SetDefault("WARNING: This item is not real!\nIf you're seeing this through a cheating mod, just ignore it\nThis item simply allows the icon in the config to display, even when this item is unloaded");
+	}
+}
 
-public class _CONFIG_MysticMirror : ModItem { }
+public class _CONFIG_WarpedMirror : ConfigItem { }
 
-public class _CONFIG_RunicMirror : ModItem { }
+public class _CONFIG_MysticMirror : ConfigItem { }
 
-public class _CONFIG_SpiralMirror : ModItem { }
+public class _CONFIG_RunicMirror : ConfigItem { }
 
-public class _CONFIG_GemstoneMagnet : ModItem { }
+public class _CONFIG_SpiralMirror : ConfigItem { }
 
-public class _CONFIG_MagnificentMagnet : ModItem { }
+public class _CONFIG_GemstoneMagnet : ConfigItem { }
 
-public class _CONFIG_QuantumStrongbox : ModItem { }
+public class _CONFIG_MagnificentMagnet : ConfigItem { }
 
-public class _CONFIG_GoldenHorseshoeBalloon : ModItem { }
+public class _CONFIG_QuantumStrongbox : ConfigItem { }
 
-public class _CONFIG_FlowerOfTheJungle : ModItem { }
+public class _CONFIG_GoldenHorseshoeBalloon : ConfigItem { }
 
-public class _CONFIG_PrefixHammers : ModItem { }
+public class _CONFIG_FlowerOfTheJungle : ConfigItem { }
+
+public class _CONFIG_PrefixHammers : ConfigItem { }
