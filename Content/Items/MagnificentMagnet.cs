71bb227fed4f95acf12df6a9ff6486e1be66b357
stormytuna <stormytuna@outlook.com>
2023-06-14 16:43:39 +0100

    Fix an issue with loading then unloading

--- Content/Items/MagnificentMagnet.cs
+++ Content/Items/MagnificentMagnet.cs
@@ -14,12 +14,14 @@ public class MagnificentMagnet : ModItem
 {
 	public override bool IsLoadingEnabled(Mod mod) => ServerConfig.Instance.MagnificentMagnet;
 
-	public override void SetStaticDefaults() {
+	public override void SetStaticDefaults()
+	{
 		CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 1;
 		// Tooltip.SetDefault("Increases pickup range for items while in your inventory\nUse to pull all items in the world to you");
 	}
 
-	public override void SetDefaults() {
+	public override void SetDefaults()
+	{
 		Item.width = 26;
 		Item.height = 28;
 		Item.rare = ItemRarityID.Pink;
@@ -31,7 +33,8 @@ public class MagnificentMagnet : ModItem
 		Item.UseSound = SoundID.Item4;
 	}
 
-	public override void AddRecipes() {
+	public override void AddRecipes()
+	{
 		CreateRecipe()
 			.AddIngredient(ModContent.ItemType<GemstoneMagnet>())
 			.AddIngredient(ItemID.SoulofSight, 3)
@@ -43,7 +46,8 @@ public class MagnificentMagnet : ModItem
 
 	public int Mode { get; private set; }
 
-	public override void ModifyTooltips(List<TooltipLine> tooltips) {
+	public override void ModifyTooltips(List<TooltipLine> tooltips)
+	{
 		// Get index
 		TooltipLine lastTip = tooltips.FirstOrDefault(t => t.Name == "Tooltip1");
 		int index = tooltips.IndexOf(lastTip) + 1;
@@ -52,7 +56,8 @@ public class MagnificentMagnet : ModItem
 		// Make TooltipLine
 		string text = "";
 		Color color;
-		switch (Mode) {
+		switch (Mode)
+		{
 			case 0:
 				color = Color.Red * colorMult;
 				text = $"[c/{color.Hex3()}:Disabled]";
@@ -82,19 +87,25 @@ public class MagnificentMagnet : ModItem
 
 	public override void OnConsumeItem(Player player) => Item.stack++;
 
-	public override void RightClick(Player player) {
+	public override void RightClick(Player player)
+	{
 		Mode++;
-		if (Mode > 3) {
+		if (Mode > 3)
+		{
 			Mode = 0;
 		}
 	}
 
-	public override bool? UseItem(Player player) {
-		if (player.whoAmI == Main.myPlayer) {
-			for (int i = 0; i < Main.maxItems; i++) {
+	public override bool? UseItem(Player player)
+	{
+		if (player.whoAmI == Main.myPlayer)
+		{
+			for (int i = 0; i < Main.maxItems; i++)
+			{
 				Item item = Main.item[i];
 
-				if (!item.active || item.noGrabDelay != 0 || item.playerIndexTheItemIsReservedFor != player.whoAmI) {
+				if (!item.active || item.noGrabDelay != 0 || item.playerIndexTheItemIsReservedFor != player.whoAmI)
+				{
 					continue;
 				}
 
@@ -106,32 +117,34 @@ public class MagnificentMagnet : ModItem
 		return true;
 	}
 
-	public override void SaveData(TagCompound tag) {
+	public override void SaveData(TagCompound tag)
+	{
 		tag["MagnificentMagnetMode"] = Mode;
 	}
 
-	public override void LoadData(TagCompound tag) {
+	public override void LoadData(TagCompound tag)
+	{
 		Mode = tag.GetInt("MagnificentMagnetMode");
 	}
 }
 
 public class MagnificentMagnetPlayer : ModPlayer
 {
-	public override void Load() {
+	public override void Load()
+	{
 		On_Player.GetItemGrabRange += Player_GetItemGrabRange;
 	}
 
-	public override void Unload() {
-		On_Player.GetItemGrabRange += Player_GetItemGrabRange;
-	}
-
-	private int Player_GetItemGrabRange(On_Player.orig_GetItemGrabRange orig, Player self, Item item) {
+	private int Player_GetItemGrabRange(On.Terraria.Player.orig_GetItemGrabRange orig, Player self, Item item)
+	{
 		int ret = orig(self, item);
 
 		int magnet = self.FindItem(ModContent.ItemType<MagnificentMagnet>());
-		if (magnet != -1) {
+		if (magnet != -1)
+		{
 			MagnificentMagnet modItem = self.inventory[magnet].ModItem as MagnificentMagnet;
-			switch (modItem.Mode) {
+			switch (modItem.Mode)
+			{
 				case 1:
 					ret += 150;
 					break;
