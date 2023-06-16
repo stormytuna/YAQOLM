927e41933d1d8439558bc0e3dced9dd2fed7bb8b
stormytuna <stormytuna@outlook.com>
2023-06-16 12:43:56 +0100

    Add Spiral Mirror UI

---
+++ Common/Players/TeleportPlayer.cs
@@ -0,0 +1,138 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.Audio;
+using Terraria.ID;
+using Terraria.ModLoader;
+using YAQOLM.Common.Systems;
+using YAQOLM.Content.Items;
+
+namespace YAQOLM.Common.Players;
+
+public class TeleportPlayer : ModPlayer
+{
+	private const int TotalTeleportTime = 90;
+
+	private int teleportTime;
+	private int teleportStyle;
+
+	public void StartTeleport(int teleportStyle) {
+		this.teleportStyle = teleportStyle;
+		teleportTime = TotalTeleportTime;
+		SoundEngine.PlaySound(SoundID.Item6, Player.Center);
+	}
+
+	public override void PostUpdate() {
+		WarpedMirrorPlayer warpedPlayer = Player.GetModPlayer<WarpedMirrorPlayer>();
+
+		teleportTime--;
+		if (teleportTime < 0) {
+			return;
+		}
+
+		if (Main.rand.NextBool()) {
+			MakeDust(Player);
+		}
+
+		if (teleportTime == TotalTeleportTime / 2) {
+			// Dust where the player starts
+			for (int i = 0; i < 70; i++) {
+				MakeDust(Player);
+			}
+
+			// Release grappling hooks
+			Player.grappling[0] = -1;
+			Player.grapCount = 0;
+			for (int i = 0; i < Main.projectile.Length; i++) {
+				if (Main.projectile[i].active && Main.projectile[i].owner == Player.whoAmI && Main.projectile[i].aiStyle == 7) {
+					Main.projectile[i].Kill();
+				}
+			}
+
+			// Teleport the player
+			switch (teleportStyle) {
+				case 0:
+					Player.Spawn(PlayerSpawnContext.RecallFromItem);
+					break;
+				case 1:
+					// TODO: Come back here and let it teleport you to world spawn
+					break;
+				case 2:
+					Player.Teleport(warpedPlayer.deathLocation, -1); // style: -1 prevents vanilla from doing any teleport effects
+					warpedPlayer.canUseWarpedMirror = false;
+					break;
+				case 3:
+					Player.DoPotionOfReturnTeleportationAndSetTheComebackPoint();
+					break;
+				case 4:
+					if (Main.netMode == NetmodeID.SinglePlayer) {
+						Player.MagicConch();
+					} else if (Main.netMode == NetmodeID.MultiplayerClient && Main.myPlayer == Player.whoAmI) {
+						NetMessage.SendData(MessageID.RequestTeleportationByServer, -1, -1, null, 1);
+					}
+
+					break;
+				case 5:
+					if (Main.netMode == NetmodeID.SinglePlayer) {
+						Player.DemonConch();
+					} else if (Main.netMode == NetmodeID.MultiplayerClient && Main.myPlayer == Player.whoAmI) {
+						NetMessage.SendData(MessageID.RequestTeleportationByServer, -1, -1, null, 2);
+					}
+
+					break;
+			}
+
+			// Dust where the player appears
+			for (int i = 0; i < 70; i++) {
+				MakeDust(Player);
+			}
+		}
+	}
+
+	private void MakeDust(Player player) {
+		Dust d;
+		Vector2 vector;
+		switch (teleportStyle) {
+			case 0:
+				d = Dust.NewDustDirect(player.position, player.width, player.height, DustID.MagicMirror, 0f, 0f, 150, default, 1.1f);
+				d.velocity *= 0.5f;
+				return;
+			case 1:
+				d = Dust.NewDustDirect(player.position, player.width, player.height, DustID.MagicMirror, 0f, 0f, 150, default, 1.1f);
+				d.velocity *= 0.5f;
+				return;
+			case 2:
+				Dust.NewDust(player.position, player.width, player.height, DustID.Demonite, 0f, 0f, 150, default, 1.1f);
+				return;
+			case 3:
+				d = Dust.NewDustDirect(player.position, player.width, player.height, DustID.MagicMirror, 0f, 0f, 150, Color.Cyan, 1.1f);
+				d.velocity *= 0.5f;
+				return;
+			case 4:
+				vector = Vector2.UnitY.RotatedBy(player.itemAnimation * MathHelper.TwoPi / 30f) * new Vector2(15f, 0f);
+				d = Dust.NewDustPerfect(player.Bottom + vector, Dust.dustWater());
+				d.velocity.Y *= 0f;
+				d.velocity.Y -= 4.5f;
+				d.velocity.X *= 1.5f;
+				d.scale = 0.8f;
+				d.alpha = 130;
+				d.noGravity = true;
+				d.fadeIn = 1.1f;
+				return;
+			case 5:
+				vector = Vector2.UnitY.RotatedBy(player.itemAnimation * MathHelper.TwoPi / 30f) * new Vector2(15f, 0f);
+				d = Dust.NewDustPerfect(player.Bottom + vector, 35);
+				d.velocity.Y *= 0f;
+				d.velocity.Y -= 4.5f;
+				d.velocity.X *= 1.5f;
+				d.scale = 0.8f;
+				d.alpha = 130;
+				d.noGravity = true;
+				d.fadeIn = 1.1f;
+				return;
+		}
+	}
+
+	public override void UpdateDead() {
+		SpiralMirrorUiSystem.Hide();
+	}
+}
---
+++ Common/UI/MouseCenteredUIPanel.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria;
+using Terraria.GameContent.UI.Elements;
+
+namespace YAQOLM.Common.UI;
+
+public class MouseCenteredUIPanel : UIPanel
+{
+	private bool centreOnMouse;
+
+	public override void OnActivate() {
+		base.OnActivate();
+		centreOnMouse = true;
+	}
+
+	public override void Update(GameTime gameTime) {
+		base.Update(gameTime);
+		if (!centreOnMouse) {
+			return;
+		}
+
+		centreOnMouse = false;
+		Left.Set(Main.mouseX - Width.Pixels / 2f, 0f);
+		Top.Set(Main.mouseY - Height.Pixels / 2f, 0f);
+		Recalculate();
+	}
+
+	protected override void DrawChildren(SpriteBatch spriteBatch) {
+		if (!centreOnMouse) {
+			base.DrawChildren(spriteBatch);
+		}
+	}
+}
