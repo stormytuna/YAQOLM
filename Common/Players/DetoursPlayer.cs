50f835061c15a70ddf9efe5f09e70b646efc7e5e
stormytuna <stormytuna@outlook.com>
2023-06-16 16:26:14 +0100

    Refactor ultra fast extractinate

--- Common/Players/DetoursPlayer.cs
+++ Common/Players/DetoursPlayer.cs
@@ -1,4 +1,5 @@
-using Terraria;
+using System.Linq;
+using On.Terraria;
 using Terraria.GameContent.Achievements;
 using Terraria.ID;
 using Terraria.ModLoader;
@@ -8,71 +9,85 @@ namespace YAQOLM.Common.Players;
 
 public class DetoursPlayer : ModPlayer
 {
-	public override void Load() {
+	public override void Load()
+	{
 		// Subscribing in Load
-		if (ServerConfig.Instance.MoreAnglerLoot || ServerConfig.Instance.AnglerResetsImmediately) {
+		if (ServerConfig.Instance.MoreAnglerLoot || ServerConfig.Instance.AnglerResetsImmediately)
+		{
 			On_Player.GetAnglerReward += On_PlayerOnGetAnglerReward;
 		}
 
-		if (ServerConfig.Instance.BulkExtractinate) {
+		if (ServerConfig.Instance.BulkExtractinate)
+		{
 			On_Player.ExtractinatorUse += On_PlayerOnExtractinatorUse;
 		}
 
-		if (ServerConfig.Instance.BetterLifeFruit) {
+		if (ServerConfig.Instance.BetterLifeFruit)
+		{
 			On_Player.ItemCheck_UseLifeFruit += On_PlayerOnItemCheck_UseLifeFruit;
 		}
 	}
 
-	private void On_PlayerOnGetAnglerReward(On_Player.orig_GetAnglerReward orig, Player self, NPC angler, int questitemtype) {
-		if (ServerConfig.Instance.MoreAnglerLoot) {
+	private void Player_GetAnglerReward(Player.orig_GetAnglerReward orig, Terraria.Player self, NPC angler)
+	{
+		// Just gives us double loot from angler
+		if (ServerConfig.Instance.MoreAnglerLoot)
+		{
 			orig(self, angler, questitemtype);
 			self.anglerQuestsFinished++;
 			orig(self, angler, questitemtype);
-		} else {
+		}
+		else
+		{
 			orig(self, angler, questitemtype);
 		}
 
-		if (ServerConfig.Instance.AnglerResetsImmediately) {
+		if (ServerConfig.Instance.AnglerResetsImmediately)
+		{
 			Main.AnglerQuestSwap();
 		}
 	}
 
-	private void On_PlayerOnExtractinatorUse(On_Player.orig_ExtractinatorUse orig, Player self, int extractType, int extractinatorblocktype) {
-		while (self.HeldItem.stack > 0) {
+	private void Player_ExtractinatorUse(Player.orig_ExtractinatorUse orig, Terraria.Player self, int extractType)
+	{
+		int maxExtractinations = Main.item.SkipLast(101).Count(item => !item.active);
+
+		// Prevents us eating silt when we can't make any more items
+		if (maxExtractinations == 0)
+		{
+			self.HeldItem.stack++;
+		}
+
+		for (int i = 0; i < maxExtractinations; i++)
+		{
 			// Call orig
 			orig(self, extractType, extractinatorblocktype);
 
 			// Reduce our stack
 			self.HeldItem.stack--;
-			if (self.HeldItem.stack <= 0) {
+			if (self.HeldItem.stack <= 0)
+			{
 				self.HeldItem.TurnToAir();
-			}
-
-			// Check if we should break our loop
-			int numItems = 0;
-			for (int i = 0; i < Main.item.Length; i++) {
-				if (Main.item[i].active) {
-					numItems++;
-				}
-			}
-
-			if (numItems > 300) {
-				break;
+				return;
 			}
 		}
 	}
 
-	private void On_PlayerOnItemCheck_UseLifeFruit(On_Player.orig_ItemCheck_UseLifeFruit orig, Player self, Item sItem) {
-		if (sItem.type == ItemID.LifeFruit && self.itemAnimation > 0 && self.statLifeMax >= 400 && self.statLifeMax < 500 && self.ItemTimeIsZero) {
+	private void On_PlayerOnItemCheck_UseLifeFruit(On_Player.orig_ItemCheck_UseLifeFruit orig, Player self, Item sItem)
+	{
+		if (sItem.type == ItemID.LifeFruit && self.itemAnimation > 0 && self.statLifeMax >= 400 && self.statLifeMax < 500 && self.ItemTimeIsZero)
+		{
 			//self.ApplyItemTime(sItem);
 			self.statLifeMax += 5;
 			self.statLifeMax2 += 5;
 			self.statLife += 5;
-			if (self.statLifeMax > 500) {
+			if (self.statLifeMax > 500)
+			{
 				self.statLifeMax = 500;
 			}
 
-			if (Main.myPlayer == self.whoAmI) {
+			if (Main.myPlayer == self.whoAmI)
+			{
 				self.HealEffect(5);
 			}
 
