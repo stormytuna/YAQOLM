fca106c6f0f2d9929a111f8cd4dc9cad842348d8
stormytuna <stormytuna@outlook.com>
2023-07-02 12:58:38 +0100

    Fix possible issue?

--- Content/Items/MagnificentMagnet.cs
+++ Content/Items/MagnificentMagnet.cs
@@ -142,18 +142,20 @@ public class MagnificentMagnetPlayer : ModPlayer
 		int magnet = self.FindItem(ModContent.ItemType<MagnificentMagnet>());
 		if (magnet != -1)
 		{
-			MagnificentMagnet modItem = self.inventory[magnet].ModItem as MagnificentMagnet;
-			switch (modItem.Mode)
+			if (self.inventory[magnet].ModItem is MagnificentMagnet modItem)
 			{
-				case 1:
-					ret += 150;
-					break;
-				case 2:
-					ret += 1500;
-					break;
-				case 3:
-					ret += 15000;
-					break;
+				switch (modItem.Mode)
+				{
+					case 1:
+						ret += 150;
+						break;
+					case 2:
+						ret += 1500;
+						break;
+					case 3:
+						ret += 15000;
+						break;
+				}
 			}
 		}
 
